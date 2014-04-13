d98bcc443af25757bc467f440bed7723759d46df
Greg Heaphy <heaphyg@gmail.com>
2014-04-13 13:54:11 -0500

    completion of part 2

--- part-3-jquery-ajax/source/app/controllers/index.rb
+++ part-3-jquery-ajax/source/app/controllers/index.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
@@ -7,6 +9,16 @@ post '/color' do
 
   #Create and return a JSON object with the random cell and color given below.
 
+
+
   cell= rand(1..9)
   color= "#" + "%06x" % (rand * 0xffffff)
+
+    
+
+   {cell: cell, color: color}.to_json
+   
 end
+
+
+
--- part-3-jquery-ajax/source/public/js/application.js
+++ part-3-jquery-ajax/source/public/js/application.js
@@ -1,3 +1,37 @@
 $(document).ready(function(){
 
+	$('#get_color').click(function(event){
+		event.preventDefault();
+
+	 // var random_number = Math.floor(Math.random() * (9 - 1 + 1)) + 1
+
+	 // $('#color_me').find('li:nth-child('+ random_number + ')').css("background-color","yellow");
+
+
+		   $.post( '/color', function( response ) {
+		     console.log(response)
+		     var obj = JSON.parse(response)  // we have to convert the JSON object into javascript object
+
+	         // $( ".result" ).html( data );
+	         $('#color_me').find('li:nth-child('+ obj.cell + ')').css("background-color", obj.color);  // notice that the obj.color is not in a string as opposed to 'yellow'
+	       });
+	});
 });
+
+
+
+
+//   $('#color_me').find('li:first-child').css("background-color","yellow");
+
+
+// Math.floor(Math.random() * (9 - 1 + 1)) + 1
+
+
+//  $('#color_me').find('li:nth-child(3)').css("background-color","yellow");
+
+
+
+// $('#color_me').find('li:nth-child(random_number)').css("background-color","yellow");
+
+
+
